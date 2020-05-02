ca09c2a7fce6ae30ed650862b9e4f66c41b47739
Gadir1903 <gadir.ibragimov@bk.ru>
2020-05-02 14:47:21 +0400

    first commitT2

---
+++ java.js
@@ -0,0 +1,18 @@
+function insert(num){
+    document.form.textview.value=document.form.textview.value+num
+}
+
+
+function equal(){
+    var exp = document.form.textview.value ;
+    if(exp){
+        document.form.textview.value = eval(exp) ;
+
+    }
+}
+
+function c(){
+    document.form.textview.value="" ;
+}
+
+          
